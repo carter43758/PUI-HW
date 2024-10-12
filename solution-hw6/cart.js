058cc103ef18d7e8d7fb0af08221aef322af6569
carter43758 <cfowen@andrew.cmu.edu>
2024-10-12 10:58:55 -0400

    modified:   solution-hw6/cart.js

--- solution-hw6/cart.js
+++ solution-hw6/cart.js
@@ -1,5 +1,3 @@
-
-
 //reflecting new cart with roll info
 function createItem(newRoll) {
     const template = document.querySelector('.originals3');
@@ -87,36 +85,3 @@ function removeItems(newRoll) {
     updatePrice();
     saveToLocalStorage();
 }
-
-const rolls = {
-    "Original": {
-        name: "Original Cinnamon Roll",
-        basePrice: 2.49,
-        imageFile: "original-cinnamon-roll.jpg"
-    },
-    "Apple": {
-        name: "Apple Cinnamon Roll",
-        basePrice: 3.49,
-        imageFile: "apple-cinnamon-roll.jpg"
-    },
-    "Raisin": {
-        name: "Raisin Cinnamon Roll",
-        basePrice: 2.99,
-        imageFile: "raisin-cinnamon-roll.jpg"
-    },
-    "Walnut": {
-        name: "Walnut Cinnamon Roll",
-        basePrice: 3.49,
-        imageFile: "walnut-cinnamon-roll.jpg"
-    },
-    "Double-Chocolate": {
-        name: "Double-Chocolate Cinnamon Roll",
-        basePrice: 3.99,
-        imageFile: "double-chocolate-cinnamon-roll.jpg"
-    },
-    "Strawberry": {
-        name: "Strawberry Cinnamon Roll",
-        basePrice: 3.99,
-        imageFile: "strawberry-cinnamon-roll.jpg"
-    }    
-};
