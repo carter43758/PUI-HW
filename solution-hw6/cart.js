3e52d16eb369df131e2d06096a10743e3f3e222a
carter43758 <cfowen@andrew.cmu.edu>
2024-10-13 10:22:53 -0400

    modified:   solution-hw6/cart.js

--- solution-hw6/cart.js
+++ solution-hw6/cart.js
@@ -63,13 +63,13 @@ function updateItems(newRoll) {
 
 function calculatePrice(newRoll) {
     for (let i = 0; i < glazingOptions.length; i++) {
-        if (selectGlaze.name === glazingOptions[i].name) {
+        if (selectGlaze.value === glazingOptions[i].name) {
             glazePrice = glazingOptions[i].priceAdaptation;
         }
     }
 
     for (let i = 0; i < packSizeOptions.length; i++) {
-        if (selectPack.name.toString() === packSizeOptions[i].name) {
+        if (selectPack.value.toString() === packSizeOptions[i].name) {
             packPrice = packSizeOptions[i].multiplier;
         }
     }
