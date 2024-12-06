e4db5ba8890b17b21e920eff19d81d204868ea29
Carter Owen <cfowen@andrew.cmu.edu>
2024-12-06 18:25:59 -0500

    Update neighbor.js

--- final-project/neighbor/neighbor.js
+++ final-project/neighbor/neighbor.js
@@ -72,7 +72,7 @@ function populateBuildingInfo(place) {
     }
     
     else {
-      setTimeout(waitForElement, 1000); // Check again after 100 milliseconds
+      setTimeout(waitForElement, 200); // Check again after 100 milliseconds
     }
   };
 
